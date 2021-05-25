139def2af1d1cac88d4107f13c0aa8541da67867
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-25 21:13:14 +0100

    Poprawka

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-let num1 = 2;
-let num2 = 5;
+let number1 = 14;
+let number2 = 5;
 
 function largestNumber (num1, num2) {
-   if (num1 < num2)
+   if (num1 < num2) {
    return num2;
+} else (num1 > num2);
+   return num1;
 }
 
-console.log(largestNumber (num1, num2));
+console.log(largestNumber (number1, number2));
